8923a26e7f3a5cf5e5a5810aa3397acc35c42e53
bsquochoainew <bsquochoainew@gmail.com>
2015-03-18 14:24:52 +0700

    bsquochoainew

--- hocanhvanbadao/js/bsquochoai.js
+++ hocanhvanbadao/js/bsquochoai.js
@@ -2,6 +2,6 @@ $(function(){
 	$.ajax({
 	  url: "https://drquochoai.github.io/hocanhvanbadao/json/leftmenu.json"
 	}).success(function(re) {
-	  console.log(re);
+	  console.log(re.trangchu);
 	});
 })
