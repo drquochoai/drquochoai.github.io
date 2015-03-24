66f766fdffd78549669d90eeb3dbd08f52c2f45f
bsquochoainew <bsquochoainew@gmail.com>
2015-03-24 19:33:36 +0700

    Su dung VIP tienganh123 la bat hop phap

--- commit.cmd
+++ commit.cmd
@@ -1,4 +1,4 @@
 ﻿git add .
-git update-index --no-assume-unchanged ihatecoccoc/i_hate_coccoc_bsquochoainew_v208.8_HUY.VIP.TIENGANH123.zip
+git update-index --no-assume-unchanged /ihatecoccoc/i_hate_coccoc_bsquochoainew_v208.8_HUY.VIP.TIENGANH123.zip
 git commit -am  "Su dung VIP tienganh123 la bat hop phap"
 git push origin master
